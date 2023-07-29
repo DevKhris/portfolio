bc82ee15a82b82efa70b4b2195e9fd77cf70444a
Christian H <contact@devkhris.com>
2023-07-29 15:57:30 -0600

    feat: added profile schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,4 @@
-export const schemaTypes = []
+import profile from "./profile";
+
+export const schemaTypes = [profile];
+
---
+++ schemas/profile.ts
@@ -0,0 +1,100 @@
+import { defineField } from "sanity";
+import { BiUser } from "react-icons/bi";
+
+const profile = {
+  name: "profile",
+  title: "Profile",
+  type: "document",
+  icon: BiUser,
+  fields: [
+    defineField({
+      name: "fullName",
+      title: "Full Name",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "headline",
+      title: "Headline",
+      type: "string",
+      description: "Short description of what you do",
+      validation: (rule) => rule.required().min(40).max(60),
+    }),
+    defineField({
+      name: "profileImage",
+      title: "Profile image",
+      type: "image",
+      description: "Upload a profile picture",
+      options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    }),
+    defineField({
+      name: "shortBio",
+      title: "Short Bio",
+      type: "text",
+      rows: 4,
+    }),
+    defineField({
+      name: "email",
+      title: "Email Address",
+      type: "string",
+    }),
+    defineField({
+      name: "location",
+      title: "Location",
+      type: "string",
+    }),
+    defineField({
+      name: "fullBio",
+      title: "Full Biography",
+      type: "array",
+      of: [{ type: "block" }],
+    }),
+    defineField({
+      name: "socialLinks",
+      title: "Social Networks",
+      type: "object",
+      description: "Add your social media links:",
+      fields: [
+        {
+          name: "github",
+          title: "GitHub URL",
+          type: "url",
+          initialValue: "https://github.com/",
+        },
+        {
+          name: "linkedin",
+          title: "Linkedin URL",
+          type: "url",
+          initialValue: "https://linkedin.com/in/",
+        },
+        {
+          name: "twitter",
+          title: "Twitter URL",
+          type: "url",
+          initialValue: "https://twitter.com/",
+        },
+      ],
+      options: {
+        collapsed: false,
+        collapsible: true,
+        columns: 2,
+      },
+    }),
+    defineField({
+      name: "skills",
+      title: "Skills",
+      type: "array",
+      description: "Add a list of skills",
+      of: [{ type: "string" }],
+    }),
+  ],
+};
+
+export default profile;
