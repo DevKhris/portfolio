5b19c4830a56073d194f2892f2125d94b915f460
Christian H <contact@devkhris.com>
2023-07-31 04:06:22 -0600

    feat: added home page and layouts

---
+++ app/components/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer(props: unknown) {
+  return (
+    <footer className="border-t border-zinc-800 mt-44">
+      <div className="max-w-7x1 mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400">
+        <small className="duration-200 font-mono">
+          All rights reserved &copy; {new Date().getFullYear()}
+        </small>
+
+        <small className="hover:text-white duration-200">
+          <a href="" target="_blank" rel="noreferrer noopener">
+            Developed by{" "}
+            <span className="text-yellow-300 hover:text-yellow-500 ">
+              Christian Hernandez
+            </span>
+          </a>
+        </small>
+      </div>
+    </footer>
+  );
+}
--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -2,7 +2,7 @@ import { groq } from "next-sanity";
 import client from "./sanity.client";
 
 export async function getProfile() {
-  return client.fetch(groq`*[_type == "profile]{
+  return client.fetch(groq`*[_type == "profile"]{
     _id,
     fullName,
     headline,
