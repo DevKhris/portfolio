369a0f724f1f9765923a7726d2dff1ea81ee10ce
Christian H <contact@devkhris.com>
2023-08-21 06:06:04 -0600

    mend

--- app/(backoffice)/backoffice/[[...index]]/page.tsx
+++ app/(backoffice)/backoffice/[[...index]]/page.tsx
@@ -4,10 +4,6 @@ import { metadata as studioMeta } from "next-sanity/studio/metadata";
 import { NextStudio } from "next-sanity/studio";
 import config from "@/sanity.config";
 
-export const metadata: Metadata = {
-  ...studioMeta,
-  viewport: `${studioMeta.viewport}`,
-};
 
 export default function BackOffice() {
   return (
