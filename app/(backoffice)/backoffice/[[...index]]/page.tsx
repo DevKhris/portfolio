0b8cd5a8b6bf08e48f2e2db963ed32aaf0a197ad
Christian H <contact@devkhris.com>
2023-08-23 07:49:37 -0600

    Removed metadata from backoffice

--- app/(backoffice)/backoffice/[[...index]]/page.tsx
+++ app/(backoffice)/backoffice/[[...index]]/page.tsx
@@ -1,12 +1,6 @@
 "use client";
-import type { Metadata } from "next";
-import { metadata as studioMeta } from "next-sanity/studio/metadata";
 import { NextStudio } from "next-sanity/studio";
 import config from "@/sanity.config";
-
-export const metadata: Metadata = {
-  ...studioMeta,
-};
 
 export default function BackOffice() {
   return (
